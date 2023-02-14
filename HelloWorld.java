39b6ad467aebfb610e55815cddb9064bae8ff46b
thisiscpta <121990524+thisiscpta@users.noreply.github.com>
2023-02-14 09:46:52 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
     */
 
    public static void main(String []args) {
-      System.out.println("Hello World"); // prints Hello World
+      System.out.println("Hello World 1"); // prints Hello World
    }
 }
    
